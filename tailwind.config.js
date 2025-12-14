b435fb97ddbb3b7bc1280cdf95d2814a2bec889d
zhangsan <ffei7527@gmail.com>
2025-12-14 17:55:08 +0800

    修复：添加 index.html 和 Tailwind 配置文件

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,8 @@ export default {
     "./index.html",
     "./index.tsx",
     "./App.tsx",
-    "./src/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
